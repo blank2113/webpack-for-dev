d5366dd03df9a321539890b1a47c818ed54541f2
blank2113 <hollywoodthere@gmail.com>
2022-11-18 20:54:46 +0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,167 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack");
+
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const filename = (ext) =>
+  isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+const optimization = () => {
+  const configObj = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
+
+  if (isProd) {
+    configObj.minimizer = [new CssMinimizerPlugin(), new TerserPlugin()];
+  }
+
+  return configObj;
+};
+
+const plugins = () => {
+  const basePlugins = [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+      filename: "index.html",
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: `./css/${filename("css")}`,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets"),
+          to: path.resolve(__dirname, "app/assets"),
+        },
+      ],
+    }),
+  ];
+
+  if (isProd) {
+    basePlugins.push(
+      // Make sure that the plugin is after any plugins that add images, example `CopyWebpackPlugin`
+      new ImageminPlugin({
+        bail: false, // Ignore errors on corrupted images
+        cache: true,
+        imageminOptions: {
+          // Before using imagemin plugins make sure you have added them in `package.json` (`devDependencies`) and installed them
+
+          // Lossless optimization with custom option
+          // Feel free to experiment with options for better result for you
+          plugins: [
+            ["gifsicle", { interlaced: true }],
+            ["jpegtran", { progressive: true }],
+            ["optipng", { optimizationLevel: 5 }],
+            [
+              "svgo",
+              {
+                plugins: [
+                  {
+                    removeViewBox: false,
+                  },
+                ],
+              },
+            ],
+          ],
+        },
+      })
+    );
+  }
+
+  return basePlugins;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: "./js/main.js",
+  output: {
+    filename: `./js/${filename("js")}`,
+    path: path.resolve(__dirname, "app"),
+    assetModuleFilename: "assets/img/[name][ext]",
+    // publicPath: "",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "app"),
+    },
+    compress: true,
+    port: 3000,
+    historyApiFallback: true,
+    // contentBase: path.resolve(__dirname, "app"),
+    open: true,
+    hot: true,
+  },
+  optimization: optimization(),
+  plugins: plugins(),
+  devtool: isProd ? false : "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            // options: {
+            //   publicPath: (resourcePath, context) =>{
+            //     return path.relative(path.dirname(resourcePath),context) +'./';
+            //   },
+            // }
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(?:|woff2)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: `./fonts/${filename("[ext]")}`,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
